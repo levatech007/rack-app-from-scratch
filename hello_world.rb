be839add4fadead6e20ad2dd0baddffb1af02b4c
Eva <evaliisagalen@gmail.com>
2018-04-24 17:02:02 -0700

    added test route for query string testing

--- hello_world.rb
+++ hello_world.rb
@@ -5,10 +5,13 @@ class HelloWorld
   def call(env)
     case env['REQUEST_PATH']
     when '/'
-      ['200', {"Content-Type" => 'text/html'}, ["<h1>Hello!</h1>"]]
+      ['200', {"Content-Type" => 'text/html'}, ["<h1>Welcome!</h1>"]]
     when '/inspiration'
-      piece_of_inspiration = Inspiration.new.generate    # random inspiration 
+      piece_of_inspiration = Inspiration.new.generate    # random inspiration
       ['200', {"Content-Type" => 'application/json'}, [{:inspiration => piece_of_inspiration}.to_json]]
+    when '/test'
+      ['200', {}, ["Your request: http method is #{env['REQUEST_METHOD']}
+      \n path is #{env['PATH_INFO']} \nparams is #{env['QUERY_STRING']}"] ]
     else
       [
         '404',
