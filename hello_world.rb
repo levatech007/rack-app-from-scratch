9d5202388a39a543a9875cf09bc63788a9fa53cb
Eva <evaliisagalen@gmail.com>
2018-04-24 17:46:44 -0700

    added route to return requested number of quotes

--- hello_world.rb
+++ hello_world.rb
@@ -6,12 +6,23 @@ class HelloWorld
     case env['REQUEST_PATH']
     when '/'
       ['200', {"Content-Type" => 'text/html'}, ["<h1>Welcome!</h1>"]]
-    when '/inspiration'
+
+    when '/inspiration' #returns a random inspirational quote
       piece_of_inspiration = Inspiration.new.generate    # random inspiration
       ['200', {"Content-Type" => 'application/json'}, [{:inspiration => piece_of_inspiration}.to_json]]
-    when '/test'
+
+    when '/test' #shows query params
       ['200', {}, ["Your request: http method is #{env['REQUEST_METHOD']}
       \n path is #{env['PATH_INFO']} \nparams is #{env['QUERY_STRING']}"] ]
+
+    when '/quotes' #specify how many quotes to return /quotes/number=6
+      number_requested = env['QUERY_STRING'].to_s.split("=")[1].to_i
+      quotes_to_return = []
+      number_requested.times do |qte|
+        quotes_to_return << {:quote => Inspiration.new.generate}
+      end
+      ['200', {"Content-Type" => 'application/json'}, [quotes_to_return.to_json]]
+
     else
       [
         '404',
